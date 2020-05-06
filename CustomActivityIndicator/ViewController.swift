aebf2fe757863cb24e4a573b163a3aebef8f7a22
Ruben Dias <ruben.roque.dias@gmail.com>
2020-05-06 16:36:56 +0100

    First commit.

---
+++ CustomActivityIndicator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomActivityIndicator
+//
+//  Created by Ruben Dias on 06/05/2020.
+//  Copyright © 2020 Ruben Dias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
